aab2064c19ad0bca91af1c674af58b29823e6fb4
Nicholas Gervais <nicholaspgervais@gmail.com>
2019-08-08 23:26:16 -0500

    added verification

--- app.js
+++ app.js
@@ -36,73 +36,105 @@ function trackCompare(trackA, trackB) {
     }
 }
 
+function writeResponse(res, statusCode, contentType, message) {
+    res.writeHead(statusCode, {'Content-Type': contentType});
+    res.end(message);
+}
+
+async function isNumberVerified(number) {
+    var ver = await twilioClient.verify.services(secrets.service_id)
+        .verifications(number).fetch();
+    if (ver.status === 'approved') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+async function verifyNumber(toNumber){
+    return await twilioClient.verify.services(secrets.service_id)
+            .verifications(toNumber)
+            .update({status: 'approved'});
+}
+
+async function sendSMS(fromNumber, toNumber, bodyText) {
+    return await twilioClient.messages
+    .create({
+        body: bodyText,
+        from: fromNumber,
+        to: toNumber
+    });
+}
+
 app.get('/', (req, res) => {
-    res.status(200).send("This is the lyrics to song SMS machine!");
+    writeResponse(res, 200, 'text/plain', 'This is the lyrics to song SMS machine!');
 })
 
 app.post('/sms', async (req, res) => {
     const apiKey = secrets.musixmatchApiKey;
-    let songResults = [];
-    try {
-        const lyrics = req.body.Body;
-        axios.get(`https://api.musixmatch.com/ws/1.1/track.search?q_lyrics=${lyrics}&apikey=${apiKey}`)
-            .then(response => {
-                let trackList = ((((response || {}).data || {}).message || {}).body || {}).track_list;
-                if (trackList.length == 0) {
-                    twilioClient.messages
-                    .create({
-                        body: 'Sorry we were unable to find a song result. Try a different lyric.',
-                        from: '+19528005414',
-                        to: req.body.From
-                    })
-                    .then(message => {
-                        console.log(message.sid);
-                        res.status(404).send('Sorry We were unable to find and song results');
-                    });
-                    res.status(404).send('Sorry We were unable to find and song results');
-                } else {
-                    // Sort results by track rating.
-                    trackList.sort(trackCompare);
-                    // Grab the top 3 results.
-                    songResults = trackList.slice(0, 3);
-                    
-                    // Build text response.
-                    let textResponse = `Here are the top 3 results for \"${lyrics}\":\n`;
-                    for (let i=0; i<songResults.length; i++){
-                        let track = songResults[i].track;
-                        textResponse += `${i+1}. ${track.track_name} by ${track.artist_name}.\n`;
-                    }
-                    textResponse += "\n\n Due to the trail version, you only have access to 30% of songs."
+    const fromNumber = '+19528005414';
 
-                    // Send the message to the number who sent the request!
-                    twilioClient.messages
-                    .create({
-                        body: textResponse,
-                        from: '+19528005414',
-                        to: req.body.From
-                    })
-                    .then(message => {
-                        console.log(message.sid);
-                        res.status(200).send(req.body);
-                    });
-                }
-            })
-            .catch(error => {
-                console.log(error);
-                twilioClient.messages
-                .create({
-                    body: 'Sorry, something went wrong',
-                    from: '+19528005414',
-                    to: req.body.From
-                })
-                .then(message => {
-                    console.log(message.sid);
-                    res.status(500).send('Sorry, something went wrong');
-                });
-            });
+    const toNumber = ((req || {}).body || {}).From;
+    if (!toNumber) {
+        writeResponse(res, 404, 'text/plain', 'Phone number not provided.');
+        return;
+    }
+    const lyrics = ((req || {}).body || {}).Body;
+    if (!lyrics) {
+        writeResponse(res, 404, 'text/plain', 'Lyrics not provided.');
+        return;
+    }
+
+    try {
+        if (!isNumberVerified(toNumber, req.body.sid)) {
+            await verifyNumber(toNumber);
+            console.log(`Verified: ${number}`)
+            await sendSms(fromNumber, toNumber, 'Your number has been verified. Now you can send lyrics!');
+            writeResponse(res, 200, 'text/plain', 'verified number');
+            return;
+        }
     } catch(error) {
         console.log(error);
-        res.status(500).send('Sorry, something went wrong');
+        await sendSMS(fromNumber, toNumber, 'An error occured when verifying your number');
+        writeResponse(res, 500, 'text/plain', 'Error occured during verification.');
+        return;
+    }
+
+    let songResults = [];
+    try {
+        var response = await axios.get(`https://api.musixmatch.com/ws/1.1/track.search?q_lyrics=${lyrics}&apikey=${apiKey}`)
+        let trackList = ((((response || {}).data || {}).message || {}).body || {}).track_list;
+        if (trackList.length == 0) {
+            await sendSMS(fromNumber, toNumber, 'Sorry we were unable to find a song result. Try different lyrics.');
+            writeResponse(res, 404, 'text/plain', 'Sorry we were unable to find a song result. Try different lyrics.')
+            return;
+        } else {
+            // Sort results by track rating.
+            trackList.sort(trackCompare);
+            // Grab the top 3 results.
+            songResults = trackList.slice(0, 3);
+
+            // Build text response.
+            let textResponse = `Here are the top 3 results for \"${lyrics}\":\n`;
+            for (let i=0; i<songResults.length; i++){
+                let track = songResults[i].track;
+                textResponse += `${i+1}. ${track.track_name} by ${track.artist_name}.\n`;
+            }
+            textResponse += "\n\n Due to the trail version, you only have access to 30% of songs."
+
+            // Send the message to the number who sent the request!
+            console.log(`From: ${fromNumber}`);
+            console.log(`Body: ${lyrics}`);
+            console.log(`To: ${toNumber}`);
+            await sendSMS(fromNumber, toNumber, textResponse);
+            writeResponse(res, 200, 'text/plain', 'message sent.');
+            return;
+        }
+    } catch(error) {
+        console.log(error);
+        await sendSMS(fromNumber, toNumber, 'Sorry something went wrong.');
+        writeResponse(res, 500, 'text/plain', 'Sorry something went wrong.');
+        return;
     }
 });
 
